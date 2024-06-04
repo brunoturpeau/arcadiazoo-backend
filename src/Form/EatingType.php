c2225d5a61b3dae2176692c0ad6d1bd247db852a
Bruno Turpeau <brunoturpeau99@gmail.com>
2024-06-04 22:00:29 +0200

    styles: edit eating

--- src/Form/EatingType.php
+++ src/Form/EatingType.php
@@ -6,6 +6,8 @@ use App\Entity\Eating;
 use App\Entity\Food;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,11 +16,16 @@ class EatingType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('feeding')
-            ->add('quantity')
+            ->add('feeding', TextType::class, [
+                'label' => 'Nourriture'
+            ])
+            ->add('quantity', IntegerType::class, [
+                'label' => 'Quantité'
+            ])
             ->add('food', EntityType::class, [
                 'class' => Food::class,
                 'choice_label' => 'id',
+                'label' => "Repas"
             ])
         ;
     }
