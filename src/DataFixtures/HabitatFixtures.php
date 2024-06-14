48c164b0dc6b498e21d9d8ea931e41934ead9587
Bruno Turpeau <brunoturpeau99@gmail.com>
2024-06-14 13:12:07 +0200

    feat: add manage comment habitat

--- src/DataFixtures/HabitatFixtures.php
+++ src/DataFixtures/HabitatFixtures.php
@@ -20,7 +20,7 @@ class HabitatFixtures extends Fixture
         $habitat->setName('Savane');
         $habitat->setSlug($this->slugger->slug($habitat->getName())->lower());
         $habitat->setDescription("La savane africaine, reconstitution souvent vue en zoos, abrite une multitude d'espèces emblématiques. Lions majestueux, éléphants imposants, girafes gracieuses, zèbres rayés, rhinocéros massifs, guépards rapides et hyènes rusées évoquent un monde sauvage captivant, offrant aux visiteurs une immersion unique dans la beauté et la diversité de la nature africaine.");
-        $habitat->setCommentHabitat($faker->text(50));
+        $habitat->setCommentHabitat("Améliorer l'accès au point 2");
         $manager->persist($habitat);
         $this->addReference('Savane', $habitat);
 
@@ -28,7 +28,7 @@ class HabitatFixtures extends Fixture
         $habitat->setName('Marais');
         $habitat->setSlug($this->slugger->slug($habitat->getName())->lower());
         $habitat->setDescription("Les marais abritent une biodiversité unique, souvent recréée dans les zoos. Crocodiles furtifs, alligators solitaires, tortues aquatiques, serpents glissants, grenouilles colorées et hérons élégants évoquent un écosystème vibrant. Ces créatures captivantes offrent aux visiteurs une exploration immersive des habitats humides et de leur fascinante faune.");
-        $habitat->setCommentHabitat($faker->text(50));
+        $habitat->setCommentHabitat("Il faudrait plus d'ombre au point 3");
         $manager->persist($habitat);
         $this->addReference('Marais', $habitat);
 
@@ -36,7 +36,7 @@ class HabitatFixtures extends Fixture
         $habitat->setName('Jungle');
         $habitat->setSlug($this->slugger->slug($habitat->getName())->lower());
         $habitat->setDescription("La jungle, source d'émerveillement, trouve son reflet dans les zoos. Lions rugissants, tigres rayés, singes espiègles, serpents sinueux, perroquets colorés et toucans exotiques évoquent une biodiversité luxuriante. Ces ambassadeurs de la jungle offrent aux visiteurs une immersion palpitante dans les mystères et la splendeur de ces habitats tropicaux.");
-        $habitat->setCommentHabitat($faker->text(50));
+        $habitat->setCommentHabitat(" ");
         $manager->persist($habitat);
         $this->addReference('Jungle', $habitat);
 
